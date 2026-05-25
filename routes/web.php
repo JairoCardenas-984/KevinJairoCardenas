d911d2e9358ad6b91d8c3501a548dee90bb9c3ec
JairoCardenas-984 <202300245h@utea.edu.pe>
2026-05-25 15:44:55 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('events',[EventController::class,'index'])->name('events.index');
+Route::post('events',[EventController::class,'store'])->name('events.store');
