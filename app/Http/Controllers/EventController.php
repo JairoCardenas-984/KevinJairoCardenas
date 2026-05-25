e16dde1258b90704071203624e421c84249fc081
JairoCardenas-984 <202300245h@utea.edu.pe>
2026-05-25 17:05:10 -0500

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,9 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Http\Requests\StoreEventRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class EventController extends Controller
 {
+
+    public function index():View
+    {
+        $events =Event::all();
+         return view('events.index',['events'=> $events]);   
+    }
+
     public function store(StoreEventRequest $request):RedirectResponse
     {
         $eventData = $request->all();
